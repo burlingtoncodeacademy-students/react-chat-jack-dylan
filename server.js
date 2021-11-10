af46ca4276a5f4136cf19d422c11285e2211cf76
Jack Lavallee <jacklavallee217@gmail.com>
2021-11-10 15:59:17 -0500

    Installed Dependencies

--- server.js
+++ server.js
@@ -8,7 +8,24 @@ const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
 app.use(express.static(staticDir));
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
 
+const chatSchema = new mongoose.Schema({
+  "when": Date,
+  "author": String,
+  "body": String
+})
+
+db.on("error", console.error.bind(console, "connection error"));
+
+app.get('/', (req, res) => {
+  res.send('Good day chat')
+})
 
 app.listen(port, () => {
   console.log('listening on port: ' + port) 
