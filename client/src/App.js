e483466d0ecde663a756ede946a7ec99908dc0f9
Jack Lavallee <jacklavallee217@gmail.com>
2021-11-11 15:26:24 -0500

    Database connectivity

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, Chat!</h1>
-    </div>
-  );
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch("/api")
+      .then(res => res.json())
+      .then((res) => {
+        console.log(res)
+        console.log(data)
+        if (data === null || res.length !== data.length) {
+          setData(res)
+        }
+        else {
+          return null
+        }
+      })
+  }, [data])
+
+  if (data !== null) {
+    return (
+      <div className="App">
+        {console.log(data)}
+        {data.map(item => {
+          return <h1>{item.author}: {item.body}</h1>
+        })}
+      </div>
+    )
+  }
+  else {
+    return null
+  }
 }
 
-export default App;
+export default App
--- server.js
+++ server.js
@@ -8,23 +8,59 @@ const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
 app.use(express.static(staticDir));
 
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/test-db", {
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/chat-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
 const db = mongoose.connection;
 
+// Chat entry schema
 const chatSchema = new mongoose.Schema({
-  "when": Date,
-  "author": String,
-  "body": String
+  when: Date,
+  author: String,
+  body: String
 })
+
+// Mongoose model for Chat
+const Chat = mongoose.model("Chat", chatSchema)
+
+// Creates a new chat object and pushes it up to the database
+async function newChat(date, author, body) {
+  let chat = new Chat({
+    when: date,
+    author: author,
+    body: body
+  })
+  
+  await chat.save();
+}
+
+// Takes all chats of Chat model and throws them into an array of objects
+async function findAll() {
+  let allChats = (await Chat.find({})).map((chat) => {
+    console.log(chat)
+    return chat
+  })
+  return allChats
+}
+
+// newChat(Date.now(), "Dylan", "Hello Database")
+
+//console.log(findAll())
 
 db.on("error", console.error.bind(console, "connection error"));
 
-app.get('/', (req, res) => {
-  res.send('Good day chat')
+// get specific roomId
+app.get('/chatRoom/:roomId', (req, res) => {
+  res.send(`We are in the ${req.params.roomId} room`)
+})
+
+// Get home page
+app.get('/api', async (req, res) => {
+  let result = await findAll({})
+  res.send(result)
 })
 
 app.listen(port, () => {
